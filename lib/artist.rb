fb77fcb5afdc703caedeb454a3dc697ae3299db5
Whittney Davis <wcdavis89@yahoo.com>
2019-12-01 21:10:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,15 @@
 class Artist
   attr_accessor :name
- 
+  
+  @@artists=[]
+  
   def initialize(name)
     @name = name
+    @@artists << name
+  end
+  
+  def self.all 
+    @@artists
   end
  
 end
