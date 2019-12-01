1d57eaa55b1be804e8b3cd1a25a39ef82e5cc627
Whittney Davis <wcdavis89@yahoo.com>
2019-12-01 23:12:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,3 +14,36 @@ class Artist
   end
 
   def add_song(song)
+        @songs << song
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+
+  def self.find(name)
+    @@all.find do |artist|
+      artist.name == name
+    end
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    @@all << artist
+    artist
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs()
+    puts @songs.collect {|x| x.name}
+  end
+
+end
