08296bf2520101fc13e2075ef27b74c5546ba586
Whittney Davis <wcdavis89@yahoo.com>
2019-12-01 21:07:25 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,9 @@
-class MP3Importer(files)
+class MP3Importer
+  attr_accessor :files 
+  def initialize
+    @files = files 
+  end
+  
   def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
   end
