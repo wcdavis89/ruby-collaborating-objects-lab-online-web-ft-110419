61873c80e28c72c5052d532337272ea240d5f221
Whittney Davis <wcdavis89@yahoo.com>
2019-12-01 23:18:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,19 +2,28 @@ class Artist
 
   attr_accessor :name, :songs
 
-  @@all = []
+  @@all = [] #array of artists instances
 
-  def initialize(name)
+  def initialize(name)#artist_name
     @name = name
-    @songs = []
+    @songs = [] #array of songs for each instance
   end
 
+  #this exposes the @@all Class Variable
   def self.all
     @@all
   end
 
   def add_song(song)
-        @songs << song
+    #called by the song instance, adds in that song instance
+    #called inside the song method #artist_name=
+    #after the song has been created, assigned a name
+    #the song class checks to see if there is an artist associated with the song, and if there isnt' one, it asks the Artist class to create an artist instance
+    #it then passes that artist instance back to the song to store as an attribute
+    #the song class then calls this method #add_song
+    #this method adds the song to the artist instances song array
+    #every artist instance has this array
+    @songs << song
   end
 
   def self.find_or_create_by_name(name)
@@ -24,6 +33,7 @@ class Artist
       self.create(name)
     end
   end
+
 
   def self.find(name)
     @@all.find do |artist|
@@ -37,12 +47,18 @@ class Artist
     artist
   end
 
+
+
   def save
     @@all << self
   end
 
+  #this method prints out an array of song names
+  #by accessing the name attribute of each song
   def print_songs()
     puts @songs.collect {|x| x.name}
   end
 
 end
+
+
